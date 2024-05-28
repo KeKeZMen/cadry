f0cda0cd8f4481b1af991884ad7fc4a59219c496
KeKeZMen <makeskex@yandex.ru>
2024-05-28 16:07:53 +0300

    organization & bracnh importes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { AuthModule } from '@auth/auth.module';
+import { BranchModule } from '@branch/branch.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { OrganizationModule } from '@organization/organization.module';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UserModule } from '@user/user.module';
 
@@ -10,6 +12,8 @@ import { UserModule } from '@user/user.module';
     UserModule,
     PrismaModule,
     AuthModule,
+    OrganizationModule,
+    BranchModule,
   ],
 })
 export class AppModule {}
