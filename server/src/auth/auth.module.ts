262f50f0cd49c855b43ce581a2f67e5850c0a662
KeKeZMen <makeskex@yandex.ru>
2024-05-28 14:20:42 +0300

    init auth module with strategies & guards

---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '@user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { options } from './config';
+import { GUARDS } from './guards';
+import { STRATEGIES } from './strategies';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, ...STRATEGIES, ...GUARDS],
+  imports: [PassportModule, JwtModule.registerAsync(options()), UserModule],
+})
+export class AuthModule {}
---
+++ server/src/auth/dto/register.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, MinLength, isString } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @MinLength(6)
+  repeatPassword: string;
+}
