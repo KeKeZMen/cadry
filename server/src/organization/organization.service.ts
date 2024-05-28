4629c89e2fda17a8d2b1f02229b8354836c8dbb1
KeKeZMen <makeskex@yandex.ru>
2024-05-28 17:11:03 +0300

    remove miss import

--- server/src/organization/organization.service.ts
+++ server/src/organization/organization.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
-import { CreateOrganizationDto, UpdateOrganizationDto } from './dto';
+import { UpdateOrganizationDto } from './dto';
 
 @Injectable()
 export class OrganizationService {
