a68f6007fb48399f53da2d34b210c0425d9ea564
KeKeZMen <makeskex@yandex.ru>
2024-05-28 16:10:38 +0300

    added org and branch to import

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -7,10 +7,18 @@ import { AuthService } from './auth.service';
 import { options } from './config';
 import { GUARDS } from './guards';
 import { STRATEGIES } from './strategies';
+import { OrganizationModule } from '@organization/organization.module';
+import { BranchModule } from '@branch/branch.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, ...STRATEGIES, ...GUARDS],
-  imports: [PassportModule, JwtModule.registerAsync(options()), UserModule],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync(options()),
+    UserModule,
+    OrganizationModule,
+    BranchModule,
+  ],
 })
 export class AuthModule {}
