e5c1255ee49f4fbf27ff6b11aa466f905170fa04
KeKeZMen <makeskex@yandex.ru>
2024-05-28 14:01:22 +0300

    global interfaces

---
+++ server/global.d.ts
@@ -0,0 +1,5 @@
+declare interface IJwtPayload {
+  id: string;
+  email: string;
+  role: string;
+}
--- server/libs/shared/src/decorators/current-user.decorator.ts
+++ server/libs/shared/src/decorators/current-user.decorator.ts
@@ -1,13 +1,12 @@
-import { IJwtPayload } from "@auth/interfaces";
-import { ExecutionContext, createParamDecorator } from "@nestjs/common";
-import { Request } from "express";
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { Request } from 'express';
 
 export const CurrentUser = createParamDecorator(
   (
     key: keyof IJwtPayload,
-    ctx: ExecutionContext
+    ctx: ExecutionContext,
   ): IJwtPayload | Partial<IJwtPayload> => {
     const req = ctx.switchToHttp().getRequest() as Request;
     return key ? req.user[key] : req.user;
-  }
+  },
 );
