a05bb559a4dc7a19e5a5e4ba3615f48895376966
KeKeZMen <makeskex@yandex.ru>
2024-05-28 14:21:29 +0300

    added config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { AuthModule } from '@auth/auth.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UserModule } from '@user/user.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    UserModule,
+    PrismaModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
