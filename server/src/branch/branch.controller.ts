5e9b61b32e60441e0023d4035807ec6e498a9f5f
KeKeZMen <makeskex@yandex.ru>
2024-05-28 16:30:34 +0300

    check can user D & U branch

--- server/src/branch/branch.controller.ts
+++ server/src/branch/branch.controller.ts
@@ -6,32 +6,79 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { BranchService } from './branch.service';
 import { CreateBranchDto } from './dto/create-branch.dto';
 import { UpdateBranchDto } from './dto/update-branch.dto';
+import { CurrentUser } from '@shared/decorators';
+import { UserService } from '@user/user.service';
 
 @Controller('branch')
 export class BranchController {
-  constructor(private readonly branchService: BranchService) {}
+  constructor(
+    private readonly branchService: BranchService,
+    private readonly userService: UserService,
+  ) {}
 
-  @Post()
-  create(@Body() createBranchDto: CreateBranchDto) {
+  @Post(':organizationId')
+  async create(
+    @Param('organizationId', ParseUUIDPipe) organizationId: string,
+    @Body() createBranchDto: CreateBranchDto,
+    @CurrentUser() currentUser: IJwtPayload,
+  ) {
+    const user = await this.userService.findOneByIdOrEmail(currentUser.id);
+
+    if (
+      user.branch.organizationId !== organizationId ||
+      (user.role !== 'Manager' && user.role !== 'Admin')
+    ) {
+      throw new UnauthorizedException();
+    }
+
     return this.branchService.create(createBranchDto);
   }
 
   @Get(':organizationId')
-  findOne(@Param('organizationId') organizationId: string) {
-    return this.branchService.findManyByOrganizationId(organizationId);
+  async findManyByOrganizationId(
+    @Param('organizationId') organizationId: string,
+  ) {
+    return await this.branchService.findManyByOrganizationId(organizationId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBranchDto: UpdateBranchDto) {
-    return this.branchService.update(id, updateBranchDto);
+  @Patch(':branchId')
+  async update(
+    @Param('branchId') branchId: string,
+    @Body() updateBranchDto: UpdateBranchDto,
+    @CurrentUser() currentUser: IJwtPayload,
+  ) {
+    const user = await this.userService.findOneByIdOrEmail(currentUser.id);
+
+    if (
+      user.branch.id !== branchId ||
+      (user.role !== 'Manager' && user.role !== 'Admin')
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    return await this.branchService.update(branchId, updateBranchDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.branchService.remove(id);
+  @Delete(':branchId')
+  async remove(
+    @Param('branchId') branchId: string,
+    @CurrentUser() currentUser: IJwtPayload,
+  ) {
+    const user = await this.userService.findOneByIdOrEmail(currentUser.id);
+
+    if (
+      user.branch.id !== branchId ||
+      (user.role !== 'Manager' && user.role !== 'Admin')
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    return await this.branchService.remove(branchId);
   }
 }
--- server/src/branch/branch.module.ts
+++ server/src/branch/branch.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BranchService } from './branch.service';
 import { BranchController } from './branch.controller';
 import { PrismaModule } from '@prisma/prisma.module';
+import { UserModule } from '@user/user.module';
 
 @Module({
   controllers: [BranchController],
   providers: [BranchService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   exports: [BranchService],
 })
 export class BranchModule {}
