7f955cd0d458020d925017a2d504960d7f2dae1d
KeKeZMen <makeskex@yandex.ru>
2024-05-29 13:12:22 +0300

    added global jwtguard

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { AuthModule } from '@auth/auth.module';
+import { JwtGuard } from '@auth/guards/jwt.guard';
 import { BranchModule } from '@branch/branch.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { OrganizationModule } from '@organization/organization.module';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UserModule } from '@user/user.module';
@@ -14,6 +16,12 @@ import { UserModule } from '@user/user.module';
     AuthModule,
     OrganizationModule,
     BranchModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
   ],
 })
 export class AppModule {}
