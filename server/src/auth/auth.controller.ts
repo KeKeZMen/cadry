ce1c861340d0281933f3c95582992c9dd7f3f08f
KeKeZMen <makeskex@yandex.ru>
2024-05-29 11:37:20 +0300

    complited auth

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,15 +1,99 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public } from '@shared/decorators';
+import { Cookie, Public, UserAgent } from '@shared/decorators';
 import { RegisterDto } from './dto';
+import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
+import { Tokens } from './interfaces';
+
+const REFRESH_TOKEN = 'refreshToken';
 
 @Public()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post('register-organization')
   async registerOrganization(@Body() registerDto: RegisterDto) {
     return await this.authService.registerOrganization(registerDto);
+  }
+
+  @Post('login')
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res() res: Response,
+    @UserAgent() userAgent: string,
+  ) {
+    const tokens = await this.authService.login(loginDto, userAgent);
+
+    if (!tokens) {
+      throw new BadRequestException('Не удалось авторизоваться');
+    }
+
+    this.setRefreshTokenToCookies(tokens, res);
+  }
+
+  @Get('refresh')
+  async refresh(
+    @Cookie(REFRESH_TOKEN) refreshToken: string,
+    @Res() res: Response,
+    @UserAgent() userAgent: string,
+  ) {
+    if (!refreshToken) {
+      throw new UnauthorizedException();
+    }
+
+    const tokens = await this.authService.refreshTokens(
+      refreshToken,
+      userAgent,
+    );
+
+    if (!tokens) {
+      throw new UnauthorizedException();
+    }
+
+    this.setRefreshTokenToCookies(tokens, res);
+  }
+
+  @Get('logout')
+  async logout(
+    @Cookie(REFRESH_TOKEN) refreshToken: string,
+    @Res() res: Response,
+  ) {
+    if (!refreshToken) {
+      res.sendStatus(HttpStatus.OK);
+      return;
+    }
+
+    await this.authService.deleteRefreshToken(refreshToken);
+
+    res.cookie(REFRESH_TOKEN, '', {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(),
+    });
+
+    res.sendStatus(HttpStatus.OK);
+  }
+
+  private setRefreshTokenToCookies(tokens: Tokens, res: Response) {
+    if (!tokens) {
+      throw new UnauthorizedException();
+    }
+
+    res.cookie(REFRESH_TOKEN, tokens.refreshToken.token, {
+      httpOnly: true,
+      sameSite: 'lax',
+      expires: new Date(tokens.refreshToken.exp),
+    });
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,13 +1,25 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from '@user/user.service';
 import { OrganizationService } from '@organization/organization.service';
 import { RegisterDto } from './dto';
+import { LoginDto } from './dto/login.dto';
+import { compareSync } from 'bcrypt';
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@prisma/prisma.service';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly organizationService: OrganizationService,
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
   ) {}
 
   async registerOrganization(registerDto: RegisterDto) {
@@ -29,6 +41,93 @@ export class AuthService {
       userId: user.id,
       email: registerDto.email,
       inn: registerDto.inn,
+    });
+  }
+
+  async login(loginDto: LoginDto, userAgent: string) {
+    const user = await this.userService.findOneByIdOrEmail(loginDto.email);
+
+    if (!user || !compareSync(loginDto.password, user.password)) {
+      throw new UnauthorizedException('Неверный логин или пароль');
+    }
+
+    return this.generateTokens(user, userAgent);
+  }
+
+  async refreshTokens(refreshToken: string, userAgent: string) {
+    const token = await this.prisma.token.findUnique({
+      where: {
+        token: refreshToken,
+      },
+    });
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    await this.prisma.token.delete({
+      where: {
+        token: refreshToken,
+      },
+    });
+
+    if (new Date(token.exp) < new Date()) {
+      throw new UnauthorizedException();
+    }
+
+    const user = await this.userService.findOneByIdOrEmail(token.userId);
+    return this.generateTokens(user, userAgent);
+  }
+
+  async deleteRefreshToken(token: string) {
+    return await this.prisma.token.delete({
+      where: {
+        token,
+      },
+    });
+  }
+
+  private async generateTokens(user: Partial<User>, userAgent: string) {
+    const accessToken =
+      'Bearer ' +
+      this.jwtService.sign({
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      });
+
+    const refreshToken = await this.getRefreshToken(user.id, userAgent);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  private async getRefreshToken(userId: string, userAgent: string) {
+    const _token = await this.prisma.token.findFirst({
+      where: {
+        userId,
+        userAgent,
+      },
+    });
+
+    const token = _token?.token ?? '';
+
+    return await this.prisma.token.upsert({
+      where: {
+        token,
+      },
+      update: {
+        exp: new Date(new Date().getTime() + 6 * 24 * 60 * 60 * 1000),
+        token: randomUUID(),
+      },
+      create: {
+        userId,
+        userAgent,
+        exp: new Date(new Date().getTime() + 6 * 24 * 60 * 60 * 1000),
+        token: randomUUID(),
+      },
     });
   }
 }
