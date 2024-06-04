3434625897fcbf2a731b90316458e02a510aa895
KeKeZMen <makeskex@yandex.ru>
2024-06-04 16:18:53 +0300

    get template xlsx

--- server/src/organization/organization.service.ts
+++ server/src/organization/organization.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '@database/database.service';
+import * as ExcelJs from "exceljs"
+import * as tmp from "tmp"
 import {
   CreateOrganizationDto,
   UpdateOrganizationDto,
   CreateOrganizationUserDto,
 } from './dto';
+import { Stream } from 'stream';
 
 @Injectable()
 export class OrganizationService {
@@ -51,6 +54,23 @@ export class OrganizationService {
         name,
       },
     });
+  }
+
+  async getDocumentTemplateStream() {
+    const workbook = new ExcelJs.stream.xlsx.WorkbookWriter({ filename: "Шаблон", useStyles: true })
+    const templateWorksheet = workbook.addWorksheet("Шаблон") 
+    const dataWorksheet = workbook.addWorksheet("Данные")
+    
+    const stream = new Stream()
+
+    stream.on("data", () => {
+      dataWorksheet.columns = [
+        { key: "lastName", header: "Фамилия" },
+        { key: "firstName", header: "Фамилия" },
+      ]
+    })
+
+    stream.on("end", () => {})
   }
 
   update(id: string, updateOrganizationDto: UpdateOrganizationDto) {
