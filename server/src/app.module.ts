a862a1bd3e264f264b0fc782f29bbf1c851b6126
KeKeZMen <makeskex@yandex.ru>
2024-06-06 15:50:13 +0300

    importing direction, profession & speciality

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,16 @@ import { AuthModule } from '@auth/auth.module';
 import { JwtGuard } from '@auth/guards/jwt.guard';
 import { BranchModule } from '@branch/branch.module';
 import { DatabaseModule } from '@database/database.module';
+import { DirectionModule } from '@direction/direction.module';
 import { EmployeeModule } from '@employee/employee.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { OrganizationModule } from '@organization/organization.module';
+import { ProfessionModule } from '@profession/profession.module';
+import { SpecialityModule } from '@speciality/speciality.module';
+import { StudentModule } from '@student/student.module';
 import { UserModule } from '@user/user.module';
-import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
@@ -20,6 +23,9 @@ import { StudentModule } from './student/student.module';
     BranchModule,
     EmployeeModule,
     StudentModule,
+    DirectionModule,
+    SpecialityModule,
+    ProfessionModule,
   ],
   providers: [
     {
