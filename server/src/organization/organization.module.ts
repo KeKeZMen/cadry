ce30e021936c28708bdd7721bcc256d0fbdca9d3
KeKeZMen <makeskex@yandex.ru>
2024-06-06 15:52:01 +0300

    importing direction & profession modules

--- server/src/organization/organization.module.ts
+++ server/src/organization/organization.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { OrganizationService } from './organization.service';
-import { OrganizationController } from './organization.controller';
 import { UserModule } from '@user/user.module';
 import { BranchModule } from '@branch/branch.module';
+import { DirectionModule } from '@direction/direction.module';
+import { OrganizationService } from './organization.service';
+import { OrganizationController } from './organization.controller';
+import { ProfessionModule } from '@profession/profession.module';
 
 @Module({
   controllers: [OrganizationController],
   providers: [OrganizationService],
-  imports: [UserModule, BranchModule],
+  imports: [UserModule, BranchModule, DirectionModule, ProfessionModule],
   exports: [OrganizationService],
 })
 export class OrganizationModule {}
