de49821278f6a139a0851312735115e9ba1efe98
KeKeZMen <makeskex@yandex.ru>
2024-07-09 10:58:18 +0300

    prepare to balancing work

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors();
 
   const configServise = app.get(ConfigService);
-  const PORT = configServise.get('SERVER_PORT');
+  const SERVER_PORT = configServise.get("SERVER_PORT");
+  const NODE_APP_INSTANSE = configServise.get("NODE_APP_INSTANCE");
 
-  await app.listen(PORT);
+  const port = NODE_APP_INSTANSE ? `300${NODE_APP_INSTANSE}` : SERVER_PORT;
+
+  await app.listen(port);
 }
 bootstrap();
