08f04fd131e046b19feca86fa20ed99d737944a6
KeKeZMen <makeskex@yandex.ru>
2024-07-09 09:26:39 +0300

    exports decorators

--- server/libs/shared/src/decorators/index.ts
+++ server/libs/shared/src/decorators/index.ts
@@ -3,3 +3,6 @@ export * from './user-agent.decorator';
 export * from './public.decorator';
 export * from './current-user.decorator';
 export * from './roles.decorator';
+export * from './employee-type.decorator';
+export * from './isINNValide';
+export * from './isPasswordMatching';
