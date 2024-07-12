dba28482f69604c3e303af3f7468f4ac85079035
KeKeZMen <makeskex@yandex.ru>
2024-07-12 15:20:59 +0300

    enable cors

--- server/apps/cadry-server/src/main.ts
+++ server/apps/cadry-server/src/main.ts
@@ -6,17 +6,23 @@ import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configServise = app.get(ConfigService);
+  const SERVER_PORT = configServise.get("SERVER_PORT");
+  const CLIENT_URL = configServise.get("CLIENT_URL");
+  const NODE_APP_INSTANSE = configServise.get("NODE_APP_INSTANCE");
+
   app.use(cookieParser());
+  app.enableCors({
+    origin: [CLIENT_URL],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors();
 
-  const configServise = app.get(ConfigService);
-  const SERVER_PORT = configServise.get("SERVER_PORT");
-  const NODE_APP_INSTANSE = configServise.get("NODE_APP_INSTANCE");
-
   const port = NODE_APP_INSTANSE ? `300${NODE_APP_INSTANSE}` : SERVER_PORT;
-
   await app.listen(port);
 }
 bootstrap();
