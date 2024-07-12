4b5897bc6e9e762d799a894c8a8382ba91e13143
KeKeZMen <makeskex@yandex.ru>
2024-07-12 15:19:57 +0300

    testing: all into mainpage

--- client/src/pages/Mainpage/index.tsx
+++ client/src/pages/Mainpage/index.tsx
@@ -1,3 +1,13 @@
+import { LoginForm } from "@features/auth/LoginForm";
+import { GetTemplateButton } from "@features/template/GetTemplateButton";
+import { UploadTemplateForm } from "@features/template/UploadTemplateForm";
+
 export default function Mainpage() {
-  return <div>Mainpage</div>;
+  return (
+    <div>
+      <LoginForm />
+      <GetTemplateButton />
+      <UploadTemplateForm />
+    </div>
+  );
 }
