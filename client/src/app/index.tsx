87ec033fbeaef5b59b0240529be0bbc8c17bd71c
KeKeZMen <makeskex@yandex.ru>
2024-07-15 12:07:29 +0300

    reauth into routing

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,19 +1,9 @@
 import { Routing } from "@pages";
 import { withProviders } from "./providers";
 
-import { useEffect } from "react";
-import { useAppDispatch } from "@shared";
-import { reauth } from "@features/auth";
-
 import "./index.css";
 
 const App = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (localStorage.getItem("token")) dispatch(reauth());
-  }, []);
-
   return <Routing />;
 };
 
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,17 @@
-import { lazy } from "react";
+import { reauth } from "@features/auth";
+import { useAppDispatch } from "@shared";
+import { lazy, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 const Mainpage = lazy(() => import("./Mainpage"));
 
 export const Routing = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) dispatch(reauth());
+  }, []);
+  
   return (
     <Routes>
       <Route path="/" element={<Mainpage />} />
