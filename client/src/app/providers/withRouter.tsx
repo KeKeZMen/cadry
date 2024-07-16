f0b9c56c9cdc3d29fd0f3dbf5efd58cd25d71a04
KeKeZMen <makeskex@yandex.ru>
2024-07-16 16:54:49 +0300

    remove layout from withRouter

--- client/src/app/providers/withRouter.tsx
+++ client/src/app/providers/withRouter.tsx
@@ -1,12 +1,9 @@
-import { Layout } from "@widgets/Layout";
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 export const withRouter = (component: () => React.ReactNode) => () =>
   (
     <BrowserRouter>
-      <Layout>
-        <Suspense>{component()}</Suspense>
-      </Layout>
+      <Suspense>{component()}</Suspense>
     </BrowserRouter>
   );
