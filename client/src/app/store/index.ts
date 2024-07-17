da6bceab80cb605a62f0957e6b23398056b1c36d
KeKeZMen <makeskex@yandex.ru>
2024-07-17 15:51:33 +0300

    added middlewares

--- client/src/app/store/index.ts
+++ client/src/app/store/index.ts
@@ -1,13 +1,19 @@
+import { jobRequestsApi } from "@entities/job-request";
+import { organizationApi } from "@entities/organization";
 import { authSlice } from "@features/auth";
 import { combineReducers, configureStore } from "@reduxjs/toolkit/react";
 
 const rootReducer = combineReducers({
   [authSlice.name]: authSlice.reducer,
+  [jobRequestsApi.reducerPath]: jobRequestsApi.reducer,
+  [organizationApi.reducerPath]: organizationApi.reducer,
 });
 
 export const makeStore = () =>
   configureStore({
     reducer: rootReducer,
+    middleware: (gDM) =>
+      gDM().concat(jobRequestsApi.middleware, organizationApi.middleware),
   });
 
 export type AppStore = ReturnType<typeof makeStore>;
