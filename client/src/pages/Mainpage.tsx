5b8e1ce71e09d093f42b7c4a8ef3a52bf4bc42b8
KeKeZMen <makeskex@yandex.ru>
2024-07-17 16:02:54 +0300

    testing datatable

--- client/src/pages/Mainpage.tsx
+++ client/src/pages/Mainpage.tsx
@@ -1,3 +1,86 @@
+import { useGetOrganizationsQuery } from "@entities/organization";
+import { CaretSortIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
+import {
+  Button,
+  DataTable,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@shared";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const columns: ColumnDef<IOrganization>[] = [
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Почта
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "inn",
+    header: () => <div className="text-right">ИНН</div>,
+    cell: ({ row }) => {
+      return (
+        <div className="text-right font-medium">{row.getValue("inn")}</div>
+      );
+    },
+  },
+  {
+    accessorKey: "userId",
+    header: () => <div className="text-right">ID Пользователя</div>,
+    cell: ({ row }) => {
+      return (
+        <div className="text-right font-medium">{row.getValue("userId")}</div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Действия</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
 export default function MainPage() {
-  return <></>;
+  const { data: organizations } = useGetOrganizationsQuery();
+
+  return (
+    <>{organizations && <DataTable data={organizations} columns={columns} />}</>
+  );
 }
