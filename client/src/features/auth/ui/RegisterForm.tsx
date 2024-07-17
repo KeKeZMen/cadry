716f7f775e2bfa7cc6caa6e5392df1ea8fed6544
KeKeZMen <makeskex@yandex.ru>
2024-07-17 15:53:24 +0300

    inn length

--- client/src/features/auth/ui/RegisterForm.tsx
+++ client/src/features/auth/ui/RegisterForm.tsx
@@ -24,12 +24,16 @@ const registerSchema = z
   .object({
     inn: z
       .string()
-      .min(10, { message: "ИНН не может быть короче 10 символов" }),
+      .min(10, { message: "ИНН не может быть короче 10 символов" })
+      .max(12, { message: "Инн не может быть длиннее 12 символов" }),
     email: z
       .string()
       .min(1, { message: "Поле не может быть пустым" })
       .email({ message: "Неверный формат почты" }),
-    password: z.string().min(1, { message: "Поле не может быть пустым" }),
+    password: z
+      .string()
+      .min(6, { message: "Поле не может короче 6 символов" })
+      .max(12, { message: "Поле не может быть длиннее 12 символов" }),
     passwordRepeat: z.string().min(1, { message: "Поле не может быть пустым" }),
   })
   .superRefine(({ password, passwordRepeat }, ctx) => {
