c2da331b0b8e41408c66c2425f355ea0d2add9fa
KeKeZMen <makeskex@yandex.ru>
2024-07-17 15:52:16 +0300

    jobreq + org interfaces

--- client/global.d.ts
+++ client/global.d.ts
@@ -8,3 +8,33 @@ declare interface IAuthResponse {
   user: IUser;
   accessToken: string;
 }
+
+declare interface IJobRequest {
+  id: string;
+  educationOrganizationId?: string;
+  workOrganizationId: string;
+  specialityId?: number;
+  professionId?: number;
+  count: number;
+  requestYear?: Date;
+  salary: number;
+  readyToMove: boolean;
+  targetAgreement: boolean;
+  isDisability: boolean;
+  quotas: boolean;
+  description: string;
+}
+
+declare interface IOrganization {
+  id: string;
+  name: string | null;
+  phoneNumber: string | null;
+  email: string;
+  web: string | null;
+  address: string | null;
+  inn: string;
+  description: string | null;
+  logoUrl: string | null;
+  type: "EducationOrganization" | "Organization";
+  userId: string;
+}
